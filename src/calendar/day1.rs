931648bcda348c7e7259034655c8d65da40fee17
Cassandra S. <sbrvhy4z@duck.com>
2023-12-01 14:29:47 +0100

    Day 1

--- src/calendar/day1.rs
+++ src/calendar/day1.rs
@@ -1,5 +1,13 @@
-use axum::Router;
+use axum::{extract::Path, routing::get, Router};
 
 pub fn router() -> Router {
-    Router::new()
+    Router::new().route("/1/*path", get(pow))
+}
+
+async fn pow(Path(p): Path<String>) -> String {
+    p.split('/')
+        .map(|c| c.parse::<i32>().unwrap())
+        .reduce(|a, b| a ^ b)
+        .map(|x| x.pow(3).to_string())
+        .unwrap()
 }
